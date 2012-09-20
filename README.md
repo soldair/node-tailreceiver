e288294f950723ab352d79be8f7ee50a80dee29b
Ryan Day <soldair@gmail.com>
2012-09-19 19:28:11 -0700

    updating readme with documentation on logrotation etc

---
+++ README.md
@@ -0,0 +1,67 @@
+# tailreceiver
+
+Get log data as json over tcp from forwardho servers. commit the data to log files unique to name and host. rotate and gzip files based on configured interval. Watch line events to publish data to stats or an event bus.
+
+## example
+this server prefers global. it is configured with substacks confuse and prefers the most local tailreceiver.json file to your current working directory
+
+```sh
+$>npm install -g tailreceiver
+$>tailreceiver
+server listening for \n delimited JSON over tcp on 5140
+
+```
+
+
+```js
+var tailreceiver = require('tailreceiver');
+
+var server = tailreceiver(config);
+
+server.on('line',function(){
+  console.log('')
+});
+
+```
+
+## api
+
+tailreceiver(config)
+  - returns tail receiver server
+  - config
+    - rotateInterval 
+      - miliseconds how often files should be rotated. files are prefixed with YYYMMDD(-NUM) num is optional which is the number rotation that day
+    - port
+      - the port to listen for log connections on
+    - dir
+      - the directory to write log files too
+
+tailserver.on
+  - line
+    - an event with a json object that came from a remote server.
+
+lineObject
+  client provided fields
+  - line
+    - the line of data. provided by the client defaults to undefined
+  - file
+    - the name of the file provided by the remote server. defaults to /unknown.log
+  - time
+    - the time the line fo data was created on the remote server. if not populated set to Date.now()
+
+  server set fields
+  - ip
+    - the remote ip that gave us the data
+  - port
+    - the report port
+  - now
+    - when we got it
+  - lname
+    - the name of the log file that it was added to
+
+  the client can add more fields the default behavior is to add server side fields to the object provided by the client.
+
+## woo hooo
+
+let me know if you find any issues or this package is useful
+
--- server.js
+++ server.js
@@ -1,75 +1,156 @@
 
 var net = require('net')
- , http = require('http')
- , EventEmitter = require('events').EventEmitter
- , fs = require('fs')
- , path = require('path')
- , crypto = require('crypto')
- ;
+, http = require('http')
+, EventEmitter = require('events').EventEmitter
+, fs = require('fs')
+, path = require('path')
+, crypto = require('crypto')
+, zlib = require('zlib')
+, rotator = require('rotator')
+//, bench = require('yunofast')();
+;
 
 
 module.exports = function(options){
   options = options||{};
+  // log rotator
+  var rotateConfig = {gzip:typeof options.rotateGzip == 'undefined'?true:options.rotateGzip};
+
+  if(options.rotateInterval) rotateConfig.interval = options.rotateInterval;
+  var tot = rotator(rotateConfig);
 
   //port
-  var tcp = options.tcp || 5140
-   , dir = options.dir || process.cwd()+'/logs/'
-   ;
+  var tcp = options.port || options.tcp || 5140
+  , server
+  , dir = options.dir || process.cwd()+'/logs/'
+  , lineEmitter = new EventEmitter()
+  , files = {}
+  , rotateBuffer = {}
+  ;
   
+  server = net.createServer(function(con){
+    // right now any client can keep sending data to lbuf
+    // with no newline and use up all the memory.
 
-  var lineEmitter = new EventEmitter();
-  var files = {};
+    var lbuf;
+    con.on('data',function(buf){
 
-  lineEmitter.on('line',function(line,ip,port){
-      var l = jsonParse(line);
-      if(!l) return;
+      //bench.start('on data buf');
 
+      var s = buf.toString('utf8');
+
+      if(lbuf && lbuf.length) {
+        s = lbuf.toString('utf8')+s;
+      }
+
+      //bench.stop('on data buf');
+
+      //bench.start('on data split');
+
+      var lines = s.trim().split("\n");
+      if(s.lastIndexOf("\n") == s.length-1) {
+        //got the whole line!
+        lbuf = ''
+      } else {
+        lbuf = lines.pop();
+      }
+
+      //bench.stop('on data split');
+
+      //bench.start('on data emit');
+
+      lineEmitter.emit('lines',lines,con.remoteAddress,con.remotePort);
+
+      //bench.stop('on data emit');
+    });
+
+    con.on('end',function(){
+      delete lbuf;
+    });
+  });
+
+  lineEmitter.on('lines',function(lines,ip,port){
+
+    //bench.start('whole line');
+
+    var lines = jsonParse('['+lines.join(',')+']')
+    ,l;
+
+    if(!lines) {
+      //bench.stop('whole line');
+      return;
+    }
+
+    for(i=0;i<lines.length;++i){
+      l = lines[i];
       if(!l.file) l.file = '/unknown.log';
       if(!l.time) l.time = Date.now();
       l.now = Date.now();
       l.ip = ip;
       l.port = port;
 
-      writeLine(l,files,dir);
-  });
+      if(l.lname && rotateBuffer[l.lname]) {
+        rotateBuffer[l.lname].push(l);
+        lines.splice(i,1);
+      }
 
-  var server = net.createServer(function(con){
-      // right now any client can keep sending data to lbuf
-      // with no newline and use up all the memory.
+      server.emit('line',l);
 
-      var lbuf;
-      con.on('data',function(buf){
-        var s = buf.toString('utf8');
-
-        if(lbuf && lbuf.length) {
-          s = lbuf.toString('utf8')+s;
-        }
-
-        //console.log('data! ',s);
-
-        var lines = s.split("\n");
-        if(s.indexOf("\n") == s.length-1) {
-          //got the whole line!
-          lbuf = ''
-        } else {
-          lbuf = lines.pop();
-        }
-
-        lines.forEach(function(line){
-          //console.log('line!: ',line);
-          lineEmitter.emit('line',line,con.remoteAddress,con.remotePort);
+      if(l.lname && !tot.logs[l.lname]) {
+        tot.addFile(l.lname,function(err,data){
+          if(err) {
+            console.log('error adding ',l.lname,'to rotator',err);
+          } else {
+            console.log('added file ',l.lname,' to rotator');
+          }
         });
-      });
+      }
+    }
 
+    if(!lines.length) {
+      //bench.stop('whole line');
+      return;
+    }
 
-      con.on('end',function(){
-        delete lbuf;
-      });
+    writeLines(lines,files,dir);
+
+    //bench.stop('whole line');
+    
   });
 
+  tot.on('rotate',function(rs,file,data){
+    var o = files[file];
+    if(!o) return;
+    // set rotate line buffer so we dont miss any events.
+    // if it takes a long time to get a close event we will
+    if(!rotateBuffer[file]) rotateBuffer[file] = [];
+    // have rotator wait for active stream to close
+    tot.rotateAfterClose(file,o.ws);
+    // end stream. i have to do this because rotator wont know how to do it for me.
+    o.ws.end();
+  });
+
+  var afterRotate =  function(file){ 
+    var lines = rotateBuffer[file];
+    delete rotateBuffer[file];
+    delete files[file];
+    
+    while(lines && lines.length) writeLine(lines.shift(),files,dir);
+  };
+
+  tot.on('rotated',afterRotate);
+  tot.on('rotate-error',function(err,file){
+    console.log('error rotating file! ',err,file);
+    // keep it going.
+    afterRotate(file);
+  });
 
   server.listen(tcp,function(){
-    console.log('server listeneing for \n delimited JSON over tcp on '+tcp);
+    console.log('server listening for \\n delimited JSON over tcp on '+tcp);
+  });
+
+  server.on('close',function(){
+    tot.stop();   
   });
 
   //make the log file directory
@@ -83,8 +164,12 @@ module.exports = function(options){
   return server;
 }
 
+//setInterval(function(){
+//    console.log(bench.report());
+//},10000);
+
 function jsonParse(json){
-  if(!json || !json.length || !json.trim().length) return undefined;
+  if(!json || !json.length || !(json+'').trim().length) return undefined;
   try{
     return JSON.parse(json);
   } catch (e) {
@@ -94,20 +179,37 @@ function jsonParse(json){
   return undefined;
 }
 
-function writeLine(l,openStreams,dir){
+
+function getFileObject(file,openStreams,dir) {
   //console.log('writeLine')
-  var o = openStreams[l.file];
-  if(!openStreams[l.file]) {
-    o = openStreams[l.file] = {};
+  var o = openStreams[file];
+  if(!openStreams[file]) {
+    o = openStreams[file] = {};
     var hash = crypto.createHash('sha1');
-    hash.update(l.file);
+    hash.update(file);
     var sha = hash.digest('hex');
-    o.lname = dir+(sha+'_'+path.basename(l.file).replace(/[^.a-z0-9_-]+/gi,'_'));
+    o.lname = dir+(path.basename(file).replace(/[^.a-z0-9_-]+/gi,'_')+'.'+sha);
     o.ws = fs.createWriteStream(o.lname,{ flags: 'a+'});
     o.started = Date.now();
   }
 
   o.updated = Date.now();
-  o.ws.write(JSON.stringify(l)+"\n");
+  return o;
+}
+
+function writeLines(lines,openStreams,dir){
+  //bench.start('write lines');
+
+  var files = {};
+  lines.forEach(function(l){
+      if(!files[l.file]) files[l.file] = "";
+      files[l.file] += JSON.stringify(l)+"\n"
+  });
+
+  Object.keys(files).forEach(function(file){  
+    var o = getFileObject(file,openStreams,dir);
+    o.ws.write(files[file]);
+  });
+  //bench.stop('write lines');
 }
 
